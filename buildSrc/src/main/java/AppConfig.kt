4a9dfc936febd7934a29ccb3eda4b2d146034b00
ahmetfarukcuha <ahmetfarukcuha@hotmail.com>
2023-04-09 20:53:43 +0300

    version

--- buildSrc/src/main/java/AppConfig.kt
+++ buildSrc/src/main/java/AppConfig.kt
@@ -1,10 +1,10 @@
 object AppConfig {
     const val compileSdk = "android-32"
-    const val versionName = "1.0.7"
+    const val versionName = "1.0.8"
     const val buildToolsVersion = "30.0.3"
     const val minSdk = 21
     const val targetSdk = 32
-    const val versionCode = 8
+    const val versionCode = 9
     const val applicationId = "com.pandorina.hal_fiyatlar"
 
     const val androidTestInstrumentation = "androidx.test.runner.AndroidJUnitRunner"
